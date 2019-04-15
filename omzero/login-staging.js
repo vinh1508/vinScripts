28c349f1c96119b36c81c0c0096d712681062b12
vinhtt <vinhtt@vitalify.asia>
2019-04-15 14:48:49 +0700

    update script

--- omzero/login-staging.js
+++ omzero/login-staging.js
@@ -3,7 +3,7 @@
 // @namespace       https://vfa-vinhtt.github.io
 // @description     get access_token
 // @version         1.0.0
-// @icon         
+// @icon
 // @author          vinhtt
 // @license         MIT
 // @connect         self
@@ -11,7 +11,22 @@
 // @include         https://data-stg-jp.omronconnect.mobi/app/oauth2-frontend/static/result.html?*
 // ==/UserScript==
 
-(function (window, document) {
-    'use strict';
-    alert(window.location.href);
+(function(window, document) {
+    "use strict";
+    let params = [];
+    window.location.href.split(/\?|\&/).forEach(item => {
+        let [key, value] = item.split("=") || [];
+        if (key === "provider_user_id" || key === "oauth_token" || key === "refresh_token") {
+            key = key.replace("provider_user_id", "ogsc_id").replace("oauth_token", "token_test");
+            params.push([key, value]);
+        }
+    });
+    let link = "";
+    params.forEach(([key, value]) => {
+        link += `${key}=${value}&`;
+    });
+    link = "https://cdn-s3-dev-apne1-hi875.zero-events.com/omwalk/index.html#/?" + link;
+    document
+        .querySelector(".alert-warning")
+        .insertAdjacentHTML("beforeEnd", `<p>Link UserApp: <a href="${link}">${link}</a><p>`);
 })(window, document);
