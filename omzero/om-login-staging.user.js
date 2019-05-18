c791d31b4fa14c9a63af979be005d73f50fdd357
vinhtt <vinhtt@vitalify.asia>
2019-05-18 11:27:35 +0700

    update link

--- omzero/om-login-staging.user.js
+++ omzero/om-login-staging.user.js
@@ -2,7 +2,7 @@
 // @name            Omzero Get Access Token
 // @namespace       https://vfa-vinhtt.github.io
 // @description     get access_token
-// @version         1.0.2
+// @version         1.0.3
 // @icon
 // @author          vinhtt
 // @license         MIT
@@ -28,7 +28,26 @@
         link += `${key}=${value}&`;
     });
     link = "https://cdn-s3-dev-apne1-hi875.zero-events.com/omwalk/index.html#/?" + link;
-    document
-        .querySelector(".alert-warning")
-        .insertAdjacentHTML("beforeEnd", `<p>Link UserApp: <a href="${link}">${link}</a><p>`);
+    window.LINK_APP = link;
+    document.querySelector(".alert-warning").insertAdjacentHTML(
+        "beforeEnd",
+        `<p>
+            <select class="sel-link-app">
+                <option value="dev">development</option>
+                <option value="local">localhost</option>
+            </select>
+        </p>
+        
+        <p>Link UserApp: <a class="link-app" href="${link}">${link}</a><p>`
+    );
+    document.querySelector(".sel-link-app").onchange = function(e) {
+        let value = e.target.value;
+        let $a = document.querySelector(".link-app");
+        let link = window.LINK_APP;
+        if(value ==='local'){
+            link = link.replace('https://cdn-s3-dev-apne1-hi875.zero-events.com/omwalk/index.html#/','http://localhost:8009/#/');
+        }
+        $a.setAttribute('href', link);
+        $a.text = link;
+    };
 })(window, document);
