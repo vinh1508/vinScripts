3e00c463ef544794b46e0b54b7d7a5443ad7c03f
vinhtt <vinhtt@vitalify.asia>
2019-06-29 11:51:48 +0700

    update script

--- omzero/om-login-staging.user.js
+++ omzero/om-login-staging.user.js
@@ -15,43 +15,52 @@
 
 (function(window, document) {
     "use strict";
-    let params = [];
-    window.location.href.split(/\?|\&/).forEach(item => {
-        let [key, value] = item.split("=") || [];
-        if (key === "provider_user_id" || key === "oauth_token" || key === "refresh_token") {
+    let includes = ["provider_user_id", "oauth_token", "refresh_token"];
+    let paramsOrigin = window.location.href
+        .split(/\?|\&/)
+        .filter(
+            value =>
+                value.split("=").length === 2 &&
+                value.split("=") &&
+                includes.includes(value.split("=")[0])
+        ).map(item => {
+            let [key, value] = item.split("=") || [];
             key = key.replace("provider_user_id", "ogsc_id").replace("oauth_token", "token_test");
-            params.push([key, value]);
-        }
-    });
-    let link = "";
-    params.forEach(([key, value]) => {
-        link += `${key}=${value}&`;
-    });
-    link = "https://cdn-s3-dev-apne1-hi875.zero-events.com/omwalk/index.html#/?" + link;
-    window.LINK_APP = link;
+            return [key, value]
+        });
+
+    let params = paramsOrigin.map(([key,value])=>`${key}=${value}`).join('&');
+    let linkLocal = `http://localhost:8009/#/?${params}`;
+    let linkDev = `https://cdn-s3-dev-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
+    let linkStg = `https://cdn-s3-stg-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
+    let linkPrd = `https://cdn-s3-prd-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
+    window.APP_PARAMS = params;
+
     document.querySelector(".alert-warning").insertAdjacentHTML(
         "beforeEnd",
-        `<p>
-            <select class="sel-link-app">
-                <option value="dev">development</option>
-                <option value="local">localhost</option>
-            </select>
-        </p>
-        
-        <p>Link UserApp: <a class="link-app" href="${link}">${link}</a><p>`
+        `<section>
+            <p>
+                <select class="sel-link-app" style="height: 30px;">
+                    <option value="${linkDev}">development</option>
+                    <option value="${linkLocal}">localhost</option>
+                    <option value="${linkStg}">staging</option>
+                    <option value="${linkPrd}">production</option>
+                </select>
+            </p>
+            <p>
+                <ul>
+                    ${paramsOrigin.map(([key,value]) => `<li><label>${key}</label>: <strong>${value}</strong></li>`).join('')}
+                </ul>
+            </p>
+            <p>Link UserApp:<br/> <a class="link-app" href="${linkDev}">${linkDev}</a><p>
+        </section>`
     );
-    
+
     document.querySelector(".sel-link-app").onchange = function(e) {
         let value = e.target.value;
         let $a = document.querySelector(".link-app");
-        let link = window.LINK_APP;
-        if (value === "local") {
-            link = link.replace(
-                "https://cdn-s3-dev-apne1-hi875.zero-events.com/omwalk/index.html#/",
-                "http://localhost:8009/#/"
-            );
-        }
-        $a.setAttribute("href", link);
-        $a.text = link;
+        $a.setAttribute("href", value);
+        $a.text = value;
     };
+    
 })(window, document);
