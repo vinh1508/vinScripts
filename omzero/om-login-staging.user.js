5d206a79d460053b42897c88232e9addc7fea7b5
VinhTT <vinhtt@vitalify.asia>
2019-08-16 09:31:51 +0700

    Update

--- omzero/om-login-staging.user.js
+++ omzero/om-login-staging.user.js
@@ -18,49 +18,69 @@
     let includes = ["provider_user_id", "oauth_token", "refresh_token"];
     let paramsOrigin = window.location.href
         .split(/\?|\&/)
-        .filter(
-            value =>
-                value.split("=").length === 2 &&
-                value.split("=") &&
-                includes.includes(value.split("=")[0])
-        ).map(item => {
+        .filter(value => value.split("=").length === 2 && value.split("=") && includes.includes(value.split("=")[0]))
+        .map(item => {
             let [key, value] = item.split("=") || [];
             key = key.replace("provider_user_id", "ogsc_id").replace("oauth_token", "token_test");
-            return [key, value]
+            return [key, value];
         });
 
-    let params = paramsOrigin.map(([key,value])=>`${key}=${value}`).join('&');
-    let linkLocal = `http://localhost:8009/#/?${params}`;
-    let linkDev = `https://cdn-s3-dev-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
-    let linkStg = `https://cdn-s3-stg-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
-    let linkPrd = `https://cdn-s3-prd-apne1-cd009.zero-events.com/user-app/index.html#/?${params}`;
+    let params = paramsOrigin.map(([key, value]) => `${key}=${value}`).join("&");
+    let linkLocal = `http://localhost:[port]/#/?${params}`;
+    let linkServer = `https://cdn-s3-[env]-apne1-[code].zero-events.com/user-app/index.html#/?${params}`;
+
     window.APP_PARAMS = params;
 
     document.querySelector(".alert-warning").insertAdjacentHTML(
         "beforeEnd",
-        `<section>
+        `
+        <section>
             <p>
-                <select class="sel-link-app" style="height: 30px;">
-                    <option value="${linkDev}">development</option>
-                    <option value="${linkLocal}">localhost</option>
-                    <option value="${linkStg}">staging</option>
-                    <option value="${linkPrd}">production</option>
+                <select class="sel-code" style="height: 30px;">
+                    <option value="hi875">Omzero (ゼロイベント)</option>
+                    <option value="cd009">CW-Wellness (チャレンジ)</option>
+                </select>
+                <select class="sel-env" style="height: 30px;">
+                    <option value="dev">development</option>
+                    <option value="local">localhost</option>
+                    <option value="stg">staging</option>
+                    <option value="prod">production</option>
                 </select>
             </p>
             <p>
                 <ul>
-                    ${paramsOrigin.map(([key,value]) => `<li><label>${key}</label>: <strong>${value}</strong></li>`).join('')}
+                    ${paramsOrigin
+                        .map(([key, value]) => `<li><label>${key}</label>=<strong>${value}</strong></li>`)
+                        .join("")}
                 </ul>
             </p>
-            <p>Link UserApp:<br/> <a class="link-app" href="${linkDev}">${linkDev}</a><p>
+            <p>Link UserApp:<br/> <a class="link-app" href="#"></a><p>
         </section>`
     );
+    function updateLink() {
+        let env = document.querySelector(".sel-env").value;
+        let code = document.querySelector(".sel-code").value;
 
-    document.querySelector(".sel-link-app").onchange = function(e) {
-        let value = e.target.value;
+        let link = linkServer.replace("[env]", env).replace("[code]", code);
+        if(env === 'local'){
+            link = linkLocal;
+            if(code === 'cd009'){
+                link = link.replace('[port]',3009);
+            } else {
+                link = link.replace('[port]',3000);
+            }
+        }
         let $a = document.querySelector(".link-app");
-        $a.setAttribute("href", value);
-        $a.text = value;
+        $a.setAttribute("href", link);
+        $a.text = link;
+    }
+    document.querySelector(".sel-code").onchange = function(e) {
+        updateLink();
     };
-    
+    document.querySelector(".sel-env").onchange = function(e) {
+        updateLink();
+    };
+    setTimeout(() => {
+        updateLink();
+    }, 1000);
 })(window, document);
